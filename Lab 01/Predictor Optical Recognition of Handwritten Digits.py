48e0d5897fa15a03e39d8fd83c6c17dbb0b71a53
Marcos Ramos <marcos.1108@hotmail.com>
2024-04-12 11:00:34 +0100

    changes

--- Lab 01/Predictor Optical Recognition of Handwritten Digits.py
+++ Lab 01/Predictor Optical Recognition of Handwritten Digits.py
@@ -1,20 +1,12 @@
 import pickle as p1
 import pandas as pd
 
-data_x = input("introduza valores para teste\n")
-data = data_x.split(",")
-print(data)
-fmap_data = map(int, data)
-print(fmap_data)
-flist_data = list(fmap_data)
-print(flist_data)
-data1 = pd.read_csv("datasets/optdigits.tes", sep=",", header=None)
-data2 = data1.iloc[:0, :64]
-data_preparation = pd.DataFrame([flist_data], columns=list(data2))
-out = data2
-for x in out:
-    print(x, data_preparation[x].values)
 loaded_model = p1.load(open('optical_recognition_of_handwritten_digits_predictor.pkl', 'rb'))
+data_x = input("Introduza valores para teste separados por vírgula:\n")
+data = data_x.split(",")
+flist_data = list(map(int, data))
+data_preparation = pd.DataFrame([flist_data])
 y_pred = loaded_model.predict(data_preparation)
 y_pred = y_pred.astype(int)
-print("Optical Recognition of Handwritten:", y_pred[0][0])
+print("Optical Recognition of Handwritten:", y_pred[0])
+
